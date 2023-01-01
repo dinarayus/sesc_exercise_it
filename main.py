722ef5240b82563f4df24b00548231f52c40e1d8
Dinara Khubiyeva <103835425+dinarayus@users.noreply.github.com>
2023-01-01 21:52:53 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import heapq
-from collections import Counter, namedtuple
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class Node(namedtuple("Node", ["left", "right"])):
-    def walk(self, code, acc):
-        self.left.walk(code, acc + "0")
-        self.right.walk(code, acc + "1")
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-class Leaf(namedtuple("Leaf", ["char"])):
-    def walk(self, code, acc):
-        code[self.char] = acc or "0"
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-
-def  huffman_encode(s):
-    h = []
-    for symbol, frequency in Counter(s).items():
-        h.append((frequency, len(h), Leaf(symbol)))
-
-    heapq.heapify(h)
-
-    count = len(h)
-    while len(h) > 1:
-        frequency1, _count1, left = heapq.heappop(h)
-        frequency2, _count2, right = heapq.heappop(h)
-        heapq.heappush(h, (frequency1 + frequency2, count, Node(left, right)))
-        count += 1
-
-    code = {}
-    if h:
-        [(_frequency, _count, root)] = h
-        root.walk(code,"")
-    return code
-
-def main():
-    s = input()
-    code = huffman_encode(s)
-    encoded = "".join(code[symbol] for symbol in s)
-    print(len(code), len(encoded))
-    for symbol in sorted(code):
-        print("{}: {}".format(symbol, code[symbol]))
-    print(encoded)
-
-if __name__ == "__main__":
-    main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
