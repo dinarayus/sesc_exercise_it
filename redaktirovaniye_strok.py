b5d73d1225d8858c8b14b1dfe8a7bd712b30db56
Dinara Khubiyeva <103835425+dinarayus@users.noreply.github.com>
2023-04-04 12:32:41 +0700

    Update redaktirovaniye_strok.py

--- redaktirovaniye_strok.py
+++ redaktirovaniye_strok.py
@@ -1,1 +1,41 @@
 strochka1 = "8" * 70
+while "2222" in strochka1 or '8888' in strochka1:
+    if "2222" in strochka1:
+        strochka1 = strochka1.replace("2222", "88")
+    if "8888" in strochka1:
+        strochka1 = strochka1.replace("8888", "22")
+print("1)",strochka1)
+
+def prost_function(num): #является ли число простым
+    for i in range(2,int(num**0.5)+1):
+        if (num%i) == 0:
+            return False
+    return True
+for n in range(1000):
+    strochka3='>' + 39*'0' + 39*'2' + n*'1'
+    while '>1' in strochka3 or '>2' in strochka3 or '>0' in strochka3:
+        if '>1' in strochka3:
+            strochka3= strochka3.replace('>1','22>')
+
+        if '>2' in strochka3:
+            strochka3= strochka3.replace('>2','2>')
+
+        if '>0' in strochka3:
+            strochka3 = strochka3.replace('>0','1>')
+
+    sum_ = 0
+    for i in strochka3[:-1]:
+        sum_ += int(i)
+    if prost_function(sum_): #проверка простого числа
+        print("3)", n)
+        break
+
+strochka4 = '>' + '1' * 11 + '2' * 12 + '3' * 30
+while ('>1' in strochka4) or ('>2' in strochka4) or ('>3' in strochka4):
+    if '>1' in strochka4:
+        strochka4 = strochka4.replace('>1', '22>')
+    if '>2' in strochka4:
+        strochka4 = strochka4.replace('>2', '2>')
+    if '>3' in strochka4:
+        strochka4 = strochka4.replace('>3', '1>')
+print('4)', strochka4.count('1') + strochka4.count('2') * 2)
